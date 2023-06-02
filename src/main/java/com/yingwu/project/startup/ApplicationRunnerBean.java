efb5b8068384fab0c6209c9f8de847b5a6bcedc9
DY56 <2535368017@qq.com>
2023-06-02 20:02:15 +0800

    修改：修改了启动的实现接口，也对加载系统接口鉴权数据的过程提取为一个方法。

---
+++ src/main/java/com/yingwu/project/startup/ApplicationRunnerBean.java
@@ -0,0 +1,17 @@
+package com.yingwu.project.startup;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Dy56
+ */
+@Component
+public class ApplicationRunnerBean implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
