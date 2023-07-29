18ebd013a8f74d0c3323ffe6d0d3870484905ac3
DY56 <2535368017@qq.com>
2023-07-29 15:09:39 +0800

    新增：新增请求日志查询。

---
+++ src/main/java/com/yingwu/project/controller/SysRequestLogController.java
@@ -0,0 +1,107 @@
+package com.yingwu.project.controller;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yingwu.project.common.BaseResponse;
+import com.yingwu.project.common.ErrorCode;
+import com.yingwu.project.common.ResultUtils;
+import com.yingwu.project.exception.ThrowUtils;
+import com.yingwu.project.model.dto.sysrequestlog.SysRequestLogQueryRequest;
+import com.yingwu.project.model.entity.SysRequestLog;
+import com.yingwu.project.service.SysRequestLogService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.yingwu.project.constant.OrderConstant.SORT_ORDER_ASC;
+import static com.yingwu.project.constant.SysConstant.MAX_PAGE_SIZE;
+
+/**
+ * 系统请求日志接口
+ *
+ * @author Dy56
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("/sysRequestLog")
+public class SysRequestLogController {
+
+    @Resource
+    private SysRequestLogService sysRequestLogService;
+
+    // region 查
+
+    /**
+     * 获取系统请求日志列表
+     *
+     * @param sysRequestLogServiceQueryRequest
+     * @param request
+     * @return
+     */
+    @GetMapping(value = "/list", name = "获取系统请求日志列表")
+    public BaseResponse<List<SysRequestLog>> listSysRequestLog(SysRequestLogQueryRequest sysRequestLogServiceQueryRequest, HttpServletRequest request) {
+        QueryWrapper<SysRequestLog> queryWrapper = buildSysRequestLogQueryWrapper(sysRequestLogServiceQueryRequest);
+        List<SysRequestLog> tradeList = sysRequestLogService.list(queryWrapper);
+        return ResultUtils.success(tradeList);
+    }
+
+    /**
+     * 分页获取系统请求日志列表
+     *
+     * @param sysRequestLogServiceQueryRequest
+     * @param request
+     * @return
+     */
+    @GetMapping(value = "/list/page", name = "分页获取系统请求日志列表")
+    public BaseResponse<Page<SysRequestLog>> listSysRequestLogByPage(SysRequestLogQueryRequest sysRequestLogServiceQueryRequest, HttpServletRequest request) {
+        long current = 1;
+        long size = 10;
+        if (sysRequestLogServiceQueryRequest != null) {
+            current = sysRequestLogServiceQueryRequest.getCurrent();
+            size = sysRequestLogServiceQueryRequest.getPageSize();
+            // 限制爬虫
+            ThrowUtils.throwIf(size > MAX_PAGE_SIZE, ErrorCode.PARAMS_ERROR);
+        }
+        QueryWrapper<SysRequestLog> queryWrapper = buildSysRequestLogQueryWrapper(sysRequestLogServiceQueryRequest);
+
+        Page<SysRequestLog> tradeListPage = sysRequestLogService.page(new Page<>(current, size), queryWrapper);
+
+        return ResultUtils.success(tradeListPage);
+    }
+
+    /**
+     * 构建系统请求日志查询条件
+     *
+     * @param sysRequestLogServiceQueryRequest
+     * @return
+     */
+    private QueryWrapper<SysRequestLog> buildSysRequestLogQueryWrapper(SysRequestLogQueryRequest sysRequestLogServiceQueryRequest) {
+        SysRequestLog sysRequestLog = new SysRequestLog();
+        if (sysRequestLogServiceQueryRequest != null) {
+            BeanUtil.copyProperties(sysRequestLogServiceQueryRequest, sysRequestLog);
+        }
+
+        QueryWrapper<SysRequestLog> queryWrapper = new QueryWrapper<>(sysRequestLog);
+
+        // 设置排序
+        List<String> orderList = new ArrayList<>();
+        orderList.add("gmt_create");
+        if (sysRequestLogServiceQueryRequest.getSortOrder().equals(SORT_ORDER_ASC)) {
+            queryWrapper.orderByAsc(orderList);
+        } else {
+            queryWrapper.orderByDesc(orderList);
+        }
+
+        return queryWrapper;
+    }
+
+    // endregion
+
+}
--- src/main/java/com/yingwu/project/model/dto/sysrequestlog/SysRequestLogQueryRequest.java
+++ src/main/java/com/yingwu/project/model/dto/sysrequestlog/SysRequestLogQueryRequest.java
@@ -1,29 +1,27 @@
-package com.yingwu.project.model.entity;
+package com.yingwu.project.model.dto.sysrequestlog;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.yingwu.project.common.PageRequest;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 import java.util.Date;
 
 /**
- * 系统请求日志
+ * 系统登录日志查询请求
  *
  * @author Dy56
- * @TableName sys_request_log
  */
-@TableName(value = "sys_request_log")
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class SysRequestLog implements Serializable {
+public class SysRequestLogQueryRequest extends PageRequest implements Serializable {
 
     /**
      * id
      */
-    @TableId(type = IdType.AUTO)
     private Long id;
 
     /**
@@ -62,12 +60,6 @@ public class SysRequestLog implements Serializable {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "zh", timezone = "GMT+8")
     private Date gmtCreate;
 
-    /**
-     * 修改时间
-     */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "zh", timezone = "GMT+8")
-    private Date gmtModified;
-
-    @TableField(exist = false)
     private static final long serialVersionUID = 1L;
+
 }
